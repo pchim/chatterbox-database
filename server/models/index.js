c081a7f4f4715089ec235d82bb4f7b7ec086f64b
Peter Chim <pchim24@gmail.com>
2016-05-13 21:14:33 -0700

    Query database to insert a message

--- server/models/index.js
+++ server/models/index.js
@@ -2,13 +2,24 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function() {}, // a function which produces all the messages
+    post: function(req) {
+      var queryString = 'INSERT INTO chat (user, message, roomname) VALUES(' + req.username + ',' + req.message + ',' + req.roomname + ')';
+      db.query(queryString, function(err) {
+    	  if (err) {
+          throw err;
+        } else {
+          console.log('Success: ', queryString);
+        }
+      });
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
+    get: function(username) {
+      var queryString = '';
+    },
     post: function () {}
   }
 };
