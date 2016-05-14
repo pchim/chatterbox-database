01d12782ab436128a3268d903b8c81f050f445b1
Peter Chim <pchim24@gmail.com>
2016-05-13 20:52:23 -0700

    Update schema to set username string as primary key

--- server/schema.sql
+++ server/schema.sql
@@ -1,28 +1,29 @@
+DROP DATABASE IF EXISTS chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
 CREATE TABLE users (
-	id INT PRIMARY KEY,
-	name VARCHAR(20)
+	name VARCHAR(20) PRIMARY KEY
 );
 
 CREATE TABLE rooms (
-	roomname VARCHAR(20) PRIMARY KEY
+	name VARCHAR(20) PRIMARY KEY
 );
 
 CREATE TABLE messages (
   /* Describe your table here.*/
   id INT PRIMARY KEY,
-  user INT,
+  user VARCHAR(20),
   roomname VARCHAR(20),
   message VARCHAR(140),
   created_at DATETIME,
   FOREIGN KEY (user)
-  	REFERENCES users(id),
+  	REFERENCES users(name),
 
   FOREIGN KEY (roomname)
-  	REFERENCES rooms(roomname)
+  	REFERENCES rooms(name)
 );
 
 /* Create other tables and define schemas for them here! */
