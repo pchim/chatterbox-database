d070a95501535822af6e6de34bbd6b620152a7c1
Peter Chim <pchim24@gmail.com>
2016-05-13 20:44:47 -0700

    Implement chat database, specs, and db connections

--- server/db/index.js
+++ server/db/index.js
@@ -3,5 +3,15 @@ var mysql = require('mysql');
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
+var dbConnection;
 
+dbConnection = mysql.createConnection({
+  user: 'root',
+  password: 'ppp',
+  database: 'chat'
+});
+
+dbConnection.connect();
+
+exports.dbConnection = dbConnection;
 
--- server/schema.sql
+++ server/schema.sql
@@ -2,8 +2,27 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE users (
+	id INT PRIMARY KEY,
+	name VARCHAR(20)
+);
+
+CREATE TABLE rooms (
+	roomname VARCHAR(20) PRIMARY KEY
+);
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INT PRIMARY KEY,
+  user INT,
+  roomname VARCHAR(20),
+  message VARCHAR(140),
+  created_at DATETIME,
+  FOREIGN KEY (user)
+  	REFERENCES users(id),
+
+  FOREIGN KEY (roomname)
+  	REFERENCES rooms(roomname)
 );
 
 /* Create other tables and define schemas for them here! */
